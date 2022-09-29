e37ef605284fc3950b746b39d0c8fb76fbca544b
HEA <hea_s@naver.com>
2022-09-29 10:07:50 +0900

    예외처리

--- AccessModifierOfClass/src/sec01_accessmodifier/EX03_AccessModifierOfClass_2/pack/AccessModifierOfClass_2.java
+++
@@ -1,9 +0,0 @@
-package sec01_accessmodifier.EX03_AccessModifierOfClass_2.pack;
-
-import sec01_accssmodifier.EX03_AccessModifierOfclass_2.pack.AA;
-
-import sec01_accssmodifier.EX03_AccessModifierOfclass_2.pack.CC;
-
-public class AccessModifierOfClass_2 {
-
-}
--- InheritanceOfInterface_1/src/sec02_inteface/EX02_InheritanceOfInterface_1/InheritanceOfInterface_1.java
+++ InheritanceOfInterface_1/src/sec02_inteface/EX02_InheritanceOfInterface_1/InheritanceOfInterface_1.java
@@ -1,5 +1,18 @@
 package sec02_inteface.EX02_InheritanceOfInterface_1;
 
-public class InheritanceOfInterface_1 {
+interface A {}
+interface B {}
 
+
+class C implements A {	
+}
+
+class D implements A, B {
+}
+
+class E extends C implements A, B {
+}
+public class InheritanceOfInterface_1 {
+	public static void main(String[] args) {
+	}
 }
